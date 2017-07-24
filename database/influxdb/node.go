ed69573d4268949d12e70f15f27764a092ad3bc6
Marcus Asshauer <marcus.asshauer@gmail.com>
2017-07-24 22:42:39 +0200

    removed previous changed site_code

--- database/influxdb/node.go
+++ database/influxdb/node.go
@@ -45,7 +45,7 @@ func buildNodeStats(node *runtime.Node) (tags models.Tags, fields models.Fields)
 
 	if nodeinfo := node.Nodeinfo; nodeinfo != nil {
 		tags.SetString("hostname", nodeinfo.Hostname)
-		tags.SetString("site_code", nodeinfo.System.SiteCode)
+//		tags.SetString("site_code", nodeinfo.System.SiteCode)
 		if owner := nodeinfo.Owner; owner != nil {
 			tags.SetString("owner", owner.Contact)
 		}
